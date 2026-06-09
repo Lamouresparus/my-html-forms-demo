744d72a57086b3665e8322d8c803d583dab737f8
love@anvil.works <love@anvil.works>
2026-06-09 14:00:13 +0000

    Created StandardPage; Edited standard-page.html

---
+++ client_code/StandardPage/__init__.py
@@ -0,0 +1,10 @@
+from ._anvil_designer import StandardPageTemplate
+from anvil import *
+
+
+class StandardPage(StandardPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    super().__init__(**properties)
+
+    # Any code you write here will run before the form opens.
