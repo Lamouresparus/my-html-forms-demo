1deb8254de4c880d0c7980bf0abc8b42d38e247e
love@anvil.works <love@anvil.works>
2026-06-12 16:40:34 +0000

    Created Form10 and client_code/Form10/form_template.html; Edited client_code/Form1/form_template.html, settings, client_code/CustomButton/form_template.html and Form1

--- client_code/Form10/__init__.py
+++ client_code/Form10/__init__.py
@@ -1,14 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Form10Template
 from anvil import *
 
 
-class Form1(Form1Template):
+class Form10(Form10Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
-
-  @handle("custom_button_1", "click")
-  def custom_button_1_click(self, **event_args):
-    alert("Clicked custom component")
